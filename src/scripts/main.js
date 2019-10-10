b0f8c319944757eae963f75c467689e3c08af9d2
Trey Suiter <trey.suiter@gmail.com>
2019-10-10 14:31:54 -0500

    completed chicken monkey function

---
+++ src/scripts/main.js
@@ -0,0 +1,12 @@
+console.log("Can your daddy do the dog?")
+
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+    console.log(currentNumber)
+    if (currentNumber % 5 === 0) {
+        console.log("Chicken!")
+    } else {
+        if (currentNumber % 7 === 0) {
+            console.log("Monkey!")
+        }
+    }
+}
